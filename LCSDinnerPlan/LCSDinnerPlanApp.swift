736eaa753d08c2d82492bc6a897f042b5fc29ad1
Logan Snopek <LSnopek@lcs.on.ca>
2021-12-07 20:10:25 -0500

    Initial Commit

---
+++ LCSDinnerPlan/LCSDinnerPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSDinnerPlanApp.swift
+//  LCSDinnerPlan
+//
+//  Created by Logan Snopek on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct LCSDinnerPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
